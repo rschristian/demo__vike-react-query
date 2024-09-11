3605dc3c4adf13ace91df0d894a63e3d4ee5a6da
Ryan Christian <rchristian@ryanchristian.dev>
2024-09-10 20:01:50 -0500

    refactor: Support cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import preact from '@preact/preset-vite'
 import vike from 'vike/plugin'
+import { pages } from "vike-cloudflare";
 
 const config = {
-  plugins: [preact(), vike()],
+  plugins: [preact(), vike(), pages()],
   // We manually add a list of dependencies to be pre-bundled, in order to avoid a page reload at dev start which breaks Vike's CI
   optimizeDeps: {
     include: ['preact', 'preact/devtools', 'preact/debug', 'preact/jsx-dev-runtime', 'preact/hooks']
